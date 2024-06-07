e57b72a55a3995f6dc8eb4cffd093447f166b9b0
Mikey Sison <85316543+mikafur32@users.noreply.github.com>
2024-06-07 18:04:23 -0500

    updated CLI tool and file saving

--- lib/evaluate.py
+++ lib/evaluate.py
@@ -51,7 +51,7 @@ mixed_precision.set_global_policy(
     policy
 )
 
-def evaluate(csv, columns, target, data_name, event_start, event_end, epochs= 10, train_test_ratio= .8, train_range= None, test_range= None, n_past= 96, n_future= 12, train_flag= True, predict_flag= True, plotstep= "Month", scaler= True):
+def evaluate(csv, saveto, columns, target, data_name, event_start, event_end, epochs= 1, train_test_ratio= .8, train_range= None, test_range= None, n_past= 96, n_future= 12, train_flag= True, plotstep= "Month", scaler= True):
 
     date = datetime.now().strftime("%B_%d_%Y_%H_%M")
 
@@ -61,44 +61,60 @@ def evaluate(csv, columns, target, data_name, event_start, event_end, epochs= 10
     else:
         train_scaled, test_scaled, train_dates, test_dates, all_dates, scaler = ingest.ingest(csv, target, renames= columns, train_range= train_range, test_range= test_range)#train_test_ratio= 0.8)
     
+    #print("+++++++++++++++++++++++++++++++++++++++++++++++++\n")
+    #print("train_scaled", train_scaled[:5])
+
     trainX, trainY = ingest.reshape(train_scaled, n_past, n_future)#, timestep_type= "hr")
     testX, testY = ingest.reshape(test_scaled,  n_past, n_future)#, timestep_type= "hr")
 
     model_names = ['Basic_LSTM', "GRU", 'Stacked_LSTM']#'Bidirectional_LSTM',]
-    def extract_segments(dir_name):
-        parts = dir_name.split("_")
-        bl_part = parts[0] + "_" + parts[1]
-        fl_part = parts[2] + "_" + parts[3][:parts[3].find('WSSVQ')]
-        return bl_part, fl_part
+    
 
-    if train_flag:
-        validation_loss_list = []
-        for model_name in model_names:
-            print(f'evaluating {model_name}')
+    validation_loss_list = []
+    for model_name in model_names:
+            if train_flag:
 
-            model = models_cuda.train_models(model_name, trainX, trainY, epochs= 10, batch_size=32, loss= "mse", load_models=False, data_name= data_name)
-            #model = models_cuda.get_model(model_name, data_name)
-            validation_loss = models_cuda.evaluate_model(model, testX, testY)
-            #seg = extract_segments(data_name)
-            #validation_loss_list.append([validation_loss, seg[0] ,seg[1] , model_name ])
+                print(f'evaluating {model_name}')
 
-            ### JUST TRAIN FOR NOW ###
-            #models_cuda.plot_model(model_name, validation_loss, data_name)
-            #K.clear_session()
-            #validation_loss_df = pd.DataFrame(validation_loss_list, columns=['Validation Loss', 'BL', 'FL', 'Model Name'])
-            #csv_path = rf"C:\Users\Mikey\Documents\Github\Hysterisis-ML-Modeling\lib\lib\model_results\VALIDATION\validation_{model_name}_{data_name}.csv"
-            #validation_loss_df.to_csv(csv_path, index=False)
-    #if predict_flag:
+                # Train
+                model = models_cuda.train_models(model_name, trainX, trainY, epochs, batch_size=32, loss= "mse", load_models=False, data_name= data_name)
+                
+                # Predictions 
+                _predict(saveto, event_start, event_end, model_name, testX, testY, test_dates, data_name, plotstep=plotstep, scaler= scaler)
 
-    _predict(event_start, event_end, model_names, testX, testY, test_dates, data_name, plotstep=plotstep, scaler= scaler)
+                # Extracting word segments from data_name for validation loss csv  
+                def extract_segments(dir_name):
+                    parts = dir_name.split("_")
+                    bl_part = parts[0] + "_" + parts[1]
+                    fl_part = parts[2] + "_" + parts[3][:parts[3].find('WSSVQ')]
+                    return bl_part, fl_part
+                
+                # Validation Loss
+                validation_loss = models_cuda.evaluate_model(model, testX, testY)
+                seg = extract_segments(data_name)
+                validation_loss_list.append([validation_loss, seg[0] ,seg[1] , model_name ])
+
+                # Plotting Validation & Training losses
+                models_cuda.plot_model(model_name, validation_loss, data_name)
+                K.clear_session()
+
+                # Saving Validation Loss to CSV
+                validation_loss_df = pd.DataFrame(validation_loss_list, columns=['Validation Loss', 'BL', 'FL', 'Model Name'])
+                csv_path = rf"{saveto}/model_results/{data_name}\{model_name}\{model_name}_{data_name}_validation.csv"
+                validation_loss_df.to_csv(csv_path, index=False)
+            else:
+                # If already trained, get model & predict
+                model = models_cuda.get_model(model_name, saveto=saveto, data=data_name)
+                _predict(saveto, event_start, event_end, model_name, testX, testY, test_dates, data_name, plotstep=plotstep, scaler= scaler)
+
+
 
 
 # MAYBE HERE needs to be edited to keep the scaled OBS values for the metrics calc
-def _predict(tstart, tend, model_names, testX, testY, test_dates, data_name, scaler=True, plotstep= "Month"):
-
+def _predict(saveto, tstart, tend, model_name, testX, testY, test_dates, data_name, scaler=True, plotstep= "Month"):
+    
     event_range = [tstart, tend]
-    print(event_range, model_names)
-    for model_name in model_names:
-        print(f"predicting {model_name} over {event_range}")
-        predicts = predict.predict(model_name, testX, data_name)
-        predict.plot_predicts(model_name, predicts, testY, test_dates, data_name, scaler=scaler ,event_range= event_range, event_plotstep= plotstep)
+    print(f"predicting {model_name} over {event_range}")
+
+    predicts = predict.predict(model_name, testX, saveto, data_name)
+    predict.plot_predicts(saveto, model_name, predicts, testY, test_dates, data_name, scaler=scaler ,event_range= event_range, event_plotstep= plotstep)
--- lib/ingest.py
+++ lib/ingest.py
@@ -1,14 +1,19 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import matplotlib.dates as mdates
 
 
 
 def read_in(csv, target, renames={}):
 
-    df = pd.read_csv(csv, low_memory=False)
+    df = pd.read_csv(csv, low_memory=False, dtype= {"0": str, 
+                                                    "1":np.float32,
+                                                    "2":np.float32,
+                                                    "3":np.float32})
     df = df.rename(columns= renames)
+    #print("+++++++++++++++++++++++++++++++++++++++++++++++++\n")
+    #print("ingest 1", df.head()) 
 
     # So much work for a oneliner HAHA
     # Reorganizes columns to be "datetime", target, feature values (renames) 
@@ -16,19 +21,26 @@ def read_in(csv, target, renames={}):
     
     df = df.set_index('datetime')
 
+    # NOTE
     df.dropna(axis = 0, inplace = True)
 
     # Reorder so that target is first.
     df = df[[target] + [x for x in renames.values() if x != target]]
 
     all_dates = df.index.to_series()
+    #print("+++++++++++++++++++++++++++++++++++++++++++++++++\n")
+
+    #print("ingest 2", df.head())
+
     return df, all_dates
+
 
 
 def train_test_split(df, train_range, test_range):
     train_from = train_range[0]
     train_to = train_range[1]
 
+    #print("+++++++++++++++++++++++++++++++++++++++++++++++++\n")
    
     #print("TEST RANGE",test_range)
 
@@ -52,11 +64,23 @@ def ingest(csv, target, n_past=96, n_future=12, renames={}, train_range= None, t
     elif(train_test_ratio):
         train_range  = [all_dates[0], all_dates.iloc[int(np.floor(len(all_dates) * train_test_ratio))]]
         test_range   = [all_dates.iloc[int(np.ceil(len(all_dates) * train_test_ratio))], all_dates[-1]]
-
+   
+    #print("+++++++++++++++++++++++++++++++++++++++++++++++++\n")
+    #print("pre-scale", df.head())
 
     if scaler:
-        scaler = StandardScaler()
-        transformed_df = scaler.fit_transform(df)
+        scaler_WL = StandardScaler()
+        scaler_Q = StandardScaler()
+        scaler_V = StandardScaler()
+        scaler_WSS = StandardScaler()
+
+        df['WL'] = scaler_WL.fit_transform(df["WL"].to_numpy().reshape(-1,1))
+        df['Q'] = scaler_Q.fit_transform(df["Q"].to_numpy().reshape(-1,1))
+        df['V'] = scaler_V.fit_transform(df["V"].to_numpy().reshape(-1,1))
+        df['WSS'] = scaler_WSS.fit_transform(df["WSS"].to_numpy().reshape(-1,1))
+
+        transformed_df = df
+
     else: 
         transformed_df = df
     
@@ -64,6 +88,9 @@ def ingest(csv, target, n_past=96, n_future=12, renames={}, train_range= None, t
     # transformed_df = pd.DataFrame(transformed_df, columns= list(renames.values()), index=df.index)
 
     transformed_df = pd.DataFrame(transformed_df, columns= df.columns, index=df.index)
+    
+    #print("+++++++++++++++++++++++++++++++++++++++++++++++++\n")
+    #print("post-scale", transformed_df.head())
 
     # Split into train and test
     train_scaled, test_scaled = train_test_split(transformed_df, train_range, test_range)
@@ -76,7 +103,7 @@ def ingest(csv, target, n_past=96, n_future=12, renames={}, train_range= None, t
     train_scaled = train_scaled.to_numpy()
     test_scaled = test_scaled.to_numpy()
 
-    return train_scaled, test_scaled, train_dates, test_dates, all_dates, scaler
+    return train_scaled, test_scaled, train_dates, test_dates, all_dates, scaler_WL
 
 def reshape(scaled, n_past, n_future, timestep_type= 'hr'):
     X = []
@@ -92,5 +119,11 @@ def reshape(scaled, n_past, n_future, timestep_type= 'hr'):
         Y.append(scaled[i + n_future - 1 : i + n_future, 0]) #0 = Discharge 
 
     X, Y = np.array(X), np.array(Y)
+
+    #print("+++++++++++++++++++++++++++++++++++++++++++++++++\n")
+    #print("X", X[0:5])
+    #print("+++++++++++++++++++++++++++++++++++++++++++++++++\n")
+    #print("Y", Y[0:5])
+
     return X, Y
 
